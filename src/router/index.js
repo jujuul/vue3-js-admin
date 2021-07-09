942e5f10bb9783c94bf2ac178a4402f3b11f3542
jujuul <576583430@qq.com>
2021-07-09 16:55:44 +0800

    配置登录路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router";
 
-const routes = [
-  {
+const routes = [{
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Login",
+    component: () =>
+      import( /* webpackChunkName: "login" */ "../views/account/Login.vue"),
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import( /* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/news",
+    name: "News",
+    component: () => import( /* webpackChunkName: "news" */ "../views/News.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
